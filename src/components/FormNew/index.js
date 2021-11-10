1ca536818b4f3317271b545c5153444d724d6d11
João Victor A <jvas1999@gmail.com>
2021-11-10 08:57:22 -0300

    tela login
    beta

--- src/components/FormNew/index.js
+++ src/components/FormNew/index.js
@@ -1,32 +1,38 @@
 import React from "react";
-import { useForm } from 'react-hook-form';
-import Button from "../Button";
 import Field from '../Field';
 import Form from '../Form';
+import { useForm } from 'react-hook-form';
+import Button from "../Button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import schema from "./Validation";
 import img2 from "./anime.png";
 import Div from "./style";
 
 const FormNew = () => {
 
-    const { register, handleSubmit, formState: { errors } } = useForm()//{
-    //     resolver: yupResolver(schema)
-    // })
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-    const onSubmit = data => console.log(data);
-
+    const onSubmit = data => {
+    console.log(data)
+    alert("Login feito com Sucesso!");
+}
+        
     const esqueceuSenha = ()=>{
-        alert("Entre em contato com o Email do Site")
+        alert("Entre em contato com o Email do Site");
     }
    
     return (
         <Div>
             <img src={img2} alt="" />
             <Form onSubmit={handleSubmit(onSubmit)}>
-                <Field.Text placeholder="Email" label="Iniciar Sessão" name="email" type="email"  register={register} />
+                <Field.Text placeholder="Email" label="Iniciar Sessão" name="email" type="email" register={register} {...register("email")} defaultValue='teste@teste.com' />
                 <span>{errors.email?.message}</span>
-                <Field.Text placeholder="Senha" label="" name="password" type="password" register={register} />
+                <Field.Text placeholder="Senha" label="" name="password" type="password" register={register} {...register("password")} defaultValue='123456' />
                 <span>{errors.password?.message}</span>
-                <Button type="submit" className='button button1'>Login</Button>
+                
+                <Button type="submit" className='button1'>Login</Button>
                 
                 <div style={{paddingTop: '45px', textAlign: 'center'}}>
                 <a href="x" onClick={esqueceuSenha} className="link">Esqueceu a senha?</a> 
